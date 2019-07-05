30ded5d46926fe759e245a0ee4f6b3eaafea9f71
Ensure happy <40486646+SuperHuYue@users.noreply.github.com>
2019-07-05 16:20:07 +0800

    若干新特性加入

--- cxxnewfeature.h
+++ cxxnewfeature.h
@@ -166,12 +166,75 @@
 
 28：正则表达式(regex expression)
 
+29:线程相关std::thread,std::mutex，std::lock_guard, std::unique_lock -->如果使用了条件变量std::condition_variable::wait则必须使用此作为参数
+   异步返回操作结果:std::future, std::packaged_task
 
+//std::future 则是提供了一个访问异步操作结果的途径，这句话很不好理解。为了理解这个特性，我们需要先理解一下在 C++11之前的多线程行为。
+//
+//试想，如果我们的主线程 A 希望新开辟一个线程 B 去执行某个我们预期的任务，并返回我一个结果。而这时候，线程 A 可能正在忙其他的事情，无暇顾及 B 的结果，所以我们会很自然的希望能够在某个特定的时间获得线程 B 的结果。
+//
+//在 C++11 的 std::future 被引入之前，通常的做法是：创建一个线程A，在线程A里启动任务 B，当准备完毕后发送一个事件，并将结果保存在全局变量中。而主函数线程 A 里正在做其他的事情，当需要结果的时候，调用一个线程等待函数来获得执行的结果。
+//
+//而 C++11 提供的 std::future 简化了这个流程，可以用来获取异步任务的结果。自然地，我们很容易能够想象到把它作为一种简单的线程同步手段。
+//
+//此外，std::packaged_task 可以用来封装任何可以调用的目标，从而用于实现异步的调用。例如：
+//
+//#include <iostream>
+//#include <future>
+//#include <thread>
+//
+//int main()
+//{
+//    // 将一个返回值为7的 lambda 表达式封装到 task 中
+//    // std::packaged_task 的模板参数为要封装函数的类型
+//    std::packaged_task<int()> task([](){return 7;});
+//    // 获得 task 的 future
+//    std::future<int> result = task.get_future(); // 在一个线程中执行 task
+//    std::thread(std::move(task)).detach(); std::cout << "Waiting...";
+//    result.wait();
+//    // 输出执行结果
+//    std::cout << "Done!" << std:: endl << "Result is " << result.get() << '\n';
+//}
+//在封装好要调用的目标后，可以使用 get_future() 来获得一个 std::future 对象，以便之后实施线程同步。
+std::condition_variable
 
+30:noexcept 表明不产生异常
 
+31:原始字符串字面量R（）省去了添加转义字符的负担
+// #include <iostream>
+// #include <string>
 
+// int main() {
+//     std::string str = R"(C:\What\The\Fxxk)";
+//     std::cout << str << std::endl;
+//     return 0;
+// }
 
+32:自定义字面量
+C++11 引进了自定义字面量的能力，通过重载双引号后缀运算符实现：
 
+//// 字符串字面量自定义必须设置如下的参数列表
+//std::string operator"" _wow1(const char *wow1, size_t len) {
+//return std::string(wow1)+"woooooooooow, amazing";
+//}
+//
+//std::string operator"" _wow2 (unsigned long long i) {
+//return std::to_string(i)+"woooooooooow, amazing";
+//}
+//
+//int main() {
+//auto str = "abc"_wow1;
+//auto num = 1_wow2;
+//std::cout << str << std::endl;
+//std::cout << num << std::endl;
+//return 0;
+//}
+//自定义字面量支持四种字面量：
+//
+//整型字面量：重载时必须使用 unsigned long long、const char *、模板字面量算符参数，在上面的代码中使用的是前者；
+//浮点型字面量：重载时必须使用 long double、const char *、模板字面量算符；
+//字符串字面量：必须使用 (const char *, size_t) 形式的参数表；
+//字符字面量：参数只能是 char, wchar_t, char16_t, char32_t 这几种类型。
 
 
 
